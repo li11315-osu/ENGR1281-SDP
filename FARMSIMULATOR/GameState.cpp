79bfd36f75a06db11f514a887d2ef5d191902ad2
Drew Ripberger <drew.ripberger@gmail.com>
2020-12-03 12:35:03 -0500

    beginning of constructor

--- FARMSIMULATOR/GameState.cpp
+++ FARMSIMULATOR/GameState.cpp
@@ -4,8 +4,31 @@
 #include <iostream>
 #include "GameState.h"
 
+#define NUMBER_OF_CROPS 12
+#define NUMBER_OF_EVENTS 5
+#define START_COINS 500
+
+const crop_type empty = {.name = "empty", .crop_id = 0};
+const crop_type carrot = {.name = "Carrot", .grow_time = 4, .crop_id = 1, .sale_price = 100, .seed_price = 20};
+
 char* GameState::test() {
    return "THIS IS A TEST";
+}
+
+// WIP
+GameState::GameState(int diff){
+
+    // Initialize state variables
+    difficulty = diff;
+    coins = START_COINS;
+    curr_day = 1;
+
+    // Set all of the plots to an empty
+    for(int i = 0; i < NUMBER_OF_CROPS; i++){
+        plots[i] = {.type = empty, .active = false};
+    }
+
+    // Initiate the events
 }
 
 #endif //GameState
--- FARMSIMULATOR/GameState.h
+++ FARMSIMULATOR/GameState.h
@@ -31,23 +31,33 @@ struct plot_raw {
 
 class GameState{
     public:
+
+        int difficulty;
+
         // The plots on the screen that can grow crops
-        plot* plots[NUMBER_OF_CROPS];
+        plot plots[NUMBER_OF_CROPS];
 
         // The random events that can occur in between days
-        event* events[NUMBER_OF_EVENTS];
-
+        event events[NUMBER_OF_EVENTS];
         int coins;
         int curr_day;
         bool stillAlive;
 
         char* test();
 
+         GameState(int);
+         
+        // Annie
         void plant(plot*, crop_type*);
+        // Annie
         void new_day();
+        // Drew
         void new_event();
-        
+
+        // Annie
         void harvest(plot*);
+
+        // Drew
         void wipeout(int[]);
 
     private:
