36f19eebce42482cea312f273ff285cb818b1554
anniegetts <74729374+anniegetts@users.noreply.github.com>
2020-12-05 17:55:54 -0600

    Update GameState.h

--- FARMSIMULATOR/GameState.h
+++ FARMSIMULATOR/GameState.h
@@ -28,6 +28,7 @@ struct event_raw {
 
 } typedef event;
 
+//Written by Drew and Annie
 struct crop_type_raw {
     char name[32];
     int grow_time;
@@ -45,6 +46,7 @@ struct crop_type_raw {
     */
 } typedef crop_type;
 
+//Written by Drew and Annie
 struct plot_raw {
     crop_type type;
     bool active;
@@ -58,15 +60,28 @@ struct plot_raw {
     */
 } typedef plot;
 
-
+//Different crop types, including a "null" crop
 const crop_type empty = crop_type{"empty", 0, 0, 0, 0};
-const crop_type carrot = crop_type{"Carrot", 4, 1, 100, 20};
+const crop_type carrot = crop_type{"Carrot", 2, 1, 100, 20};
+const crop_type tomato = crop_type{"Tomato", 4, 2, 250, 40};
+const crop_type corn = crop_type{"Corn", 3, 3, 170, 30};
+const crop_type lettuce = crop_type{"Lettuce", 5, 4, 300, 60};
+
+//Different random events, one occurs each day
 const event flood = event{"Flood", "A flash flood has destroyed some of your crops!", true, 10, std::vector<int>{0, 1, 2, 3}};
+const event tornado = event{"Tornado", "TORNADO! All of your crops are gone", true, 50, std::vector<int>{0,1,2,3,4,5,6,7,8,9,10,11}};
+const event fire = event{"Fire", "A part of the field caught fire!", true, 20, std::vector<int>{8,9,10,11}};
+const event sunny_day = event{"Sunny day :)", "Blue skies reign on this beautiful morning!", false, 40, std::vector<int>{}};
+const event thief = event{"Thief!", "Someone broke in! They stole money and may have taken some crops.", true, 100, std::vector<int>{4, 5, 6, 7}};
+const event rain = event{"Gentle rainfall", "The crops are loving the water!", false, 50, std::vector<int>{}};
+const event bug = event{"Monster bug!", "A giant bug has stomped around and damaged the farm.", true, 70, std::vector<int>{6, 7, 8, 9, 10, 11}};
+const event fertilizer = event{"Free fertilizer!", "Your neighbors have given you a gift!", false, 50, std::vector<int>{}};
+const event pandemic = event{"Cornona Virus :O", "A deadly plant virus wiped out half your plots!", true, 60, std::vector<int>{0, 1, 2, 3, 4, 5}};
 
-
-class GameState{
+//Written by Drew and Annie
+class GameState {
     public:
-
+        //Difficulty setting
         int difficulty;
 
         // The plots on the screen that can grow crops
@@ -74,24 +89,26 @@ class GameState{
 
         // The random events that can occur in between days
         event events[1] = {flood};
+
+        //Misc. game stats
         int coins;
         int curr_day;
         bool stillAlive;
 
         char* test();
 
+        //Constructor
         GameState(int);
-         
+        
+        //Methods in GameState class
         // Annie
         void plant(plot*, crop_type*);
         // Annie
         void new_day();
         // Drew
         void begin_event();
-
         // Annie
         void harvest(plot*);
-
         // Drew
         void wipeout(std::vector<int>);
 
