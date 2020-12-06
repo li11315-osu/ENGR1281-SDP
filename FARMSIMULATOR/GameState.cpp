a9b342225b16773d69aa4d251784cee5cd7688d1
anniegetts <74729374+anniegetts@users.noreply.github.com>
2020-12-05 20:01:16 -0600

    Update GameState.cpp

--- FARMSIMULATOR/GameState.cpp
+++ FARMSIMULATOR/GameState.cpp
@@ -9,6 +9,8 @@
 
 #define NUMBER_OF_PLOTS 12
 #define START_COINS 500
+//All "chaos mode" additions made by Annie
+#define CHAOS_START_COINS 400
 
 char* GameState::test() {
    return "Hello, World!";
@@ -22,12 +24,28 @@ GameState::GameState(int diff){
     coins = START_COINS;
     curr_day = 0;
 
+    //Check if player selected chaos mode
+    if (difficulty == 1) {
+       //Initialize chaos starting coin amount
+       coins = CHAOS_START_COINS;
+       //Loop through all of the possible events
+       for (int i = 0; i < 10; i++) {
+          //If the event is bad, double the monetary damage of it
+          if (events[i].isPenalty) {
+             events[i].moneyAmount *= 2;
+          //If the event is good, halve the monetary benefit of it
+          } else {
+             events[i].moneyAmount /= 2;
+          }
+       }
+    }
+
     // Set all boolean variables keeping track of what event has occurred
     // that day to false
     for(int i = 0; i < 10; i++) {
        event_occurred[i] = false;
-    } 
-   
+    }
+
     // Set all of the plots to an empty
     for(int i = 0; i < NUMBER_OF_PLOTS; i++){
         plots[i] = plot{empty, false, 0};
@@ -38,14 +56,17 @@ GameState::GameState(int diff){
 //Written by Drew
 void GameState::begin_event(){
     // Generating random number [0, number of events]
-    int pick = RandInt() % (int)(sizeof(events)/sizeof(events[0]));
+    int pick1 = RandInt() % (int)(sizeof(events)/sizeof(events[0]));
+    //Generate second random number in the case of chaos mode
+    int pick2 = RandInt() % (int)(sizeof(events)/sizeof(events[0]));
 
-    event rand_event = events[pick];
-    event_occurred[pick] = true;
+    event rand_event = events[pick1];
+    event_occurred[pick1] = true;
 
     // Deciding whether to add money or subtract it
     if(rand_event.isPenalty){
         coins -= rand_event.moneyAmount;
+        //Set the coins to 0 if they are a negative value
         if (coins < 0) {
            coins = 0;
         }
@@ -54,6 +75,25 @@ void GameState::begin_event(){
     }
 
     wipeout(rand_event.wipeout_list);
+
+   //If in chaos mode, generate a second random event (could be a repeat)
+    if (difficulty == 1) {
+       event rand_event2 = events[pick2];
+      event_occurred[pick2] = true;
+
+      // Deciding whether to add money or subtract it
+      if(rand_event2.isPenalty){
+        coins -= rand_event2.moneyAmount;
+        //Set the coins to 0 if they are a negative value
+        if (coins < 0) {
+           coins = 0;
+         }
+      } else {
+         coins += rand_event2.moneyAmount;
+      }
+
+    wipeout(rand_event2.wipeout_list);
+    }
 }
 
 //Written by Drew
